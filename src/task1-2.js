74df56d9c13acd21d203b2b3499539c7603a5496
taorkon <taorkon.tootta@gmail.com>
2023-03-24 22:50:16 +0300

    task week 10

---
+++ src/task1-2.js
@@ -0,0 +1,4 @@
+let msg = () => console.log("I'm learning JS");
+let image = document.getElementById('image');
+let nxt = () => image.src = 'assets/images/2.gif';
+let prev = () => image.src = 'assets/images/1.gif'
